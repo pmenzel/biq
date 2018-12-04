c2ff649f25cc2fe4b12a48ec6658d4b391a0ed0e
Peter Menzel <pmenzel@gmail.com>
2018-12-04 13:12:45 +0100

    Updates

---
+++ precache-manifest.55ef2eb8eb5dfe1bcf95b0bbfe898832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a777e454b4093c9cada2",
+    "url": "/biq/static/js/runtime~main.a777e454.js"
+  },
+  {
+    "revision": "5de0a4ead2a1b733ca95",
+    "url": "/biq/static/js/main.5de0a4ea.chunk.js"
+  },
+  {
+    "revision": "707fd1f45889e3ecc399",
+    "url": "/biq/static/js/1.707fd1f4.chunk.js"
+  },
+  {
+    "revision": "5de0a4ead2a1b733ca95",
+    "url": "/biq/static/css/main.a1f5643d.chunk.css"
+  },
+  {
+    "revision": "707fd1f45889e3ecc399",
+    "url": "/biq/static/css/1.af4fad73.chunk.css"
+  },
+  {
+    "revision": "b0fec9c0f10fd3f420c041865b54e299",
+    "url": "/biq/index.html"
+  }
+];
